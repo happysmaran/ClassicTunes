9a9109ac6d99b9588ca3a97db03cb6f347cf0aa0
Smaran <smaranvallabhaneni@gmail.com>
2025-10-29 17:56:40 -0400

    Initial Commit

---
+++ ClassicTunes/ClassicTunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassicTunesApp.swift
+//  ClassicTunes
+//
+//  Created by Smaran Vallabhaneni on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ClassicTunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassicTunesTests/ClassicTunesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClassicTunesTests.swift
+//  ClassicTunesTests
+//
+//  Created by Smaran Vallabhaneni on 29/10/25.
+//
+
+import Testing
+@testable import ClassicTunes
+
+struct ClassicTunesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
